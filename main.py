01986cdc905e6d4f59b382676a953d3094da0b23
Vinicios Anacleto <vinicios.l.anacleto@gmail.com>
2019-03-01 10:09:11 -0300

    List instance with states

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ import os
 # Load .env
 load_dotenv()
 # Create database connection
-db = sqlite3.connect('instances.db')
+# db = sqlite3.connect('instances.db')
 
 # Make the connection
-# ec2 = boto3.client('ec2',
-#                    aws_access_key_id=os.getenv('AWS_ID'),
-#                    aws_secret_access_key=os.getenv('AWS_SECRET'),
-#                    region_name=os.getenv('AWS_REGION'))
+ec2 = boto3.client('ec2',
+                   aws_access_key_id=os.getenv('AWS_ID'),
+                   aws_secret_access_key=os.getenv('AWS_SECRET'),
+                   region_name=os.getenv('AWS_REGION'))
 
 # Get list of instances
-# instance_list = ec2.describe_instances
+response = ec2.describe_instances()
 
+reservations = response['Reservations']
 
-db.close()
+for reserve in reservations:
+    instance = reserve['Instances'][0]
+    # print(instance)
+    print(instance['Tags'][0]['Value'],' - ', instance['InstanceId'],' [', instance['State']['Name'], ']')
+
+# db.close()
